68aaa71792d5295efd83057cdb82a0eb88b20dfc
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2025-05-08 00:17:18 +1000

    Remove .env from version control

--- .next/static/chunks/src_app_goals_page_tsx_af4bc5a1._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
-    "static/chunks/src_app_goals_page_tsx_af4bc5a1._.js",
-    {},
-]);
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-  "path": "static/chunks/src_app_goals_page_tsx_af4bc5a1._.js",
-  "chunks": [],
-  "source": "dynamic"
-});
